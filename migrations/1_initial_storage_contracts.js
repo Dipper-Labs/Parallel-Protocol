d1e9d3bb41e4670f638a1e76df74cffdee1d9bd9
cpucorecore <cpucorecore@gmail.com>
2021-04-21 15:52:33 +0800

    Escrow/EscrowStorage contract deployment

--- migrations/1_initial_storage_contracts.js
+++ migrations/1_initial_storage_contracts.js
@@ -1,11 +1,19 @@
+//basic contracts
 const Setting = artifacts.require("Setting");
 const Storage = artifacts.require("Storage");
 const AddressStorage = artifacts.require("AddressStorage");
 const Resolver = artifacts.require("Resolver");
 
-module.exports = function(deployer, network, accounts) {
-    deployer.deploy(Setting);
-    deployer.deploy(Storage);
-    deployer.deploy(AddressStorage);
-    deployer.deploy(Resolver);
+const Escrow = artifacts.require("Escrow");
+const EscrowStorage = artifacts.require("EscrowStorage");
+
+module.exports = async function(deployer, network, accounts) {
+    await deployer.deploy(Setting);
+    await deployer.deploy(Storage);
+    await deployer.deploy(AddressStorage);
+    await deployer.deploy(Resolver).then(async function() {
+        await deployer.deploy(Escrow, Resolver.address).then(async function() {
+            await deployer.deploy(EscrowStorage, Escrow.address);
+        });
+    })
 };
