ab58f4579ccaa5c66f4dcb9f8782c3fffcc33c1b
cpucorecore <cpucorecore@gmail.com>
2021-04-21 18:04:41 +0800

    SynthxToken contract deployment

--- migrations/1_initial_storage_contracts.js
+++ migrations/1_initial_storage_contracts.js
@@ -1,19 +1,46 @@
-//basic contracts
-const Setting = artifacts.require("Setting");
+const Web3Utils = require('web3-utils');
+
 const Storage = artifacts.require("Storage");
 const AddressStorage = artifacts.require("AddressStorage");
+
+const Setting = artifacts.require("Setting");
+const SettingStorage = artifacts.require("SettingStorage");
+
 const Resolver = artifacts.require("Resolver");
+
+const Issuer = artifacts.require("Issuer");
+const Holder = artifacts.require("Holder");
 
 const Escrow = artifacts.require("Escrow");
 const EscrowStorage = artifacts.require("EscrowStorage");
 
+const SynthxToken = artifacts.require("SynthxToken");
+
+const Synthx = artifacts.require("Synthx");
+
 module.exports = async function(deployer, network, accounts) {
-    await deployer.deploy(Setting);
     await deployer.deploy(Storage);
     await deployer.deploy(AddressStorage);
-    await deployer.deploy(Resolver).then(async function() {
-        await deployer.deploy(Escrow, Resolver.address).then(async function() {
-            await deployer.deploy(EscrowStorage, Escrow.address);
-        });
-    })
+    await deployer.deploy(Setting).then(async function() {
+        await deployer.deploy(SettingStorage, Setting.address);
+    });
+
+    const resolverInstance = await deployer.deploy(Resolver);
+
+    await deployer.deploy(Escrow, Resolver.address).then(async function() {
+        await deployer.deploy(EscrowStorage, Escrow.address);
+    });
+
+
+    await deployer.deploy(Issuer, Resolver.address);
+    await deployer.deploy(Holder, Resolver.address);
+
+    await resolverInstance.setAddress(Web3Utils.fromAscii('Issuer'), Issuer.address);
+    await resolverInstance.setAddress(Web3Utils.fromAscii('Holder'), Holder.address);
+
+    const synthxTokenInstance = await deployer.deploy(SynthxToken);
+    await synthxTokenInstance.initialize(Resolver.address);
+
+    // const synthxInstance = await deployer.deploy(Synthx);
+    // synthxInstance.initialize(Resolver.address, synthxTokenInstance.address);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -24,6 +24,11 @@ module.exports = {
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
+    local: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 9545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
