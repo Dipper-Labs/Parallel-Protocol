c6eedfed92a929c791ae75d0c7d195dcda9ec149
cpucorecore <cpucorecore@gmail.com>
2021-04-21 15:38:38 +0800

    Resolver contract deployment

--- migrations/1_initial_storage_contracts.js
+++ migrations/1_initial_storage_contracts.js
@@ -1,9 +1,11 @@
 const Setting = artifacts.require("Setting");
 const Storage = artifacts.require("Storage");
 const AddressStorage = artifacts.require("AddressStorage");
+const Resolver = artifacts.require("Resolver");
 
 module.exports = function(deployer, network, accounts) {
     deployer.deploy(Setting);
     deployer.deploy(Storage);
     deployer.deploy(AddressStorage);
+    deployer.deploy(Resolver);
 };
