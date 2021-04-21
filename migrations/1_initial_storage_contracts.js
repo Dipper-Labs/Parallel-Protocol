12406093310a56f9eb4416d290d411cc97064967
cpucorecore <cpucorecore@gmail.com>
2021-04-21 15:37:30 +0800

    Storage/AddressStorage contract deployment

--- migrations/1_initial_storage_contracts.js
+++ migrations/1_initial_storage_contracts.js
@@ -1,5 +1,9 @@
 const Setting = artifacts.require("Setting");
+const Storage = artifacts.require("Storage");
+const AddressStorage = artifacts.require("AddressStorage");
 
 module.exports = function(deployer, network, accounts) {
     deployer.deploy(Setting);
+    deployer.deploy(Storage);
+    deployer.deploy(AddressStorage);
 };
