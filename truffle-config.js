9efca2b1bf9096e80a73a96f461c11495278b416
Alexis <alexis@gmail.com>
2021-04-25 17:43:36 +0800

    add bsc config

--- truffle-config.js
+++ truffle-config.js
@@ -12,10 +12,24 @@ module.exports = {
       port: 8545,
       network_id: "*",
     },
+    bsctestnet: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
     ropsten: {
       provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 3,
-      gas: 3000000,
+      gas: 7865680,
       gasPrice: 10000000000
     },
   },
