1558a23a8c6ce4a3359630449e0dc2a2e430ebf8
cpucorecore <cpucorecore@gmail.com>
2021-04-25 19:16:46 +0800

    add trade testg

--- migrations/4_trade_test.js
+++ migrations/4_trade_test.js
@@ -23,70 +23,15 @@ module.exports = async function(deployer, network, accounts) {
     contracts.dTSLA = await Synth.at(contractsAddrs.dTSLA);
     contracts.dAPPLE = await Synth.at(contractsAddrs.dAPPLE);
 
-    console.log(contracts.synthx.address);
-    console.log(contracts.dUSD.address);
-    console.log(contracts.stats.address);
-    console.log(contracts.synthxDToken.address);
-    console.log(contracts.dTSLA.address);
-    console.log(contracts.dAPPLE.address);
-
-    console.log("-------- mint synths -------- ");
     await deployer
         .then(() => {
-            return contracts.synthx.mintFromCoin({value: Web3Utils.toWei('1', 'ether')});
-        })
-        .then((receipt) => {
-            console.log('synthx.mintFromCoin receipt: ', receipt);
-            return contracts.dUSD.balanceOf(accounts[0]);
-        })
-        .then((balance) => {
-            console.log("dUSD balance:", Web3Utils.fromWei(balance, 'ether'));
-            return contracts.synthxDToken.balanceOf(accounts[0]);
-        })
-        .then((balance) => {
-            console.log("dToken balance:", Web3Utils.fromWei(balance, 'ether'));
-            return contracts.stats.getTotalCollateral(accounts[0]);
-        })
-        .then((totalCollateral) => {
-            console.log("totalDebt:", Web3Utils.fromWei(totalCollateral.totalDebt, 'ether'));
-
-            console.log("\n-------- burn synths -------- ");
-            return contracts.synthx.burn(Web3Utils.fromAscii('ETH'), Web3Utils.toWei('1', 'ether'));
-        })
-        .then((receipt) => {
-            console.log('synthx.burn receipt: ', receipt);
-            return contracts.dUSD.balanceOf(accounts[0]);
-        })
-        .then((balance) => {
-            console.log("dUSD balance:", Web3Utils.fromWei(balance, 'ether'));
-            return contracts.synthxDToken.balanceOf(accounts[0]);
-        })
-        .then((balance) => {
-            console.log("dToken balance:", Web3Utils.fromWei(balance, 'ether'));
-            return contracts.stats.getTotalCollateral(accounts[0]);
-        })
-        .then((totalCollateral) => {
-            console.log("totalDebt:", Web3Utils.fromWei(totalCollateral.totalDebt, 'ether'));
-            return contracts.stats.getRewards(accounts[0]);
-        })
-        .then((reward) => {
-            console.log("rewards: ", Web3Utils.fromWei(reward, 'ether'))
-            return contracts.stats.getWithdrawable(accounts[0]);
-        })
-        .then((rewardable) => {
-            console.log("getWithdrawable:", Web3Utils.fromWei(rewardable, 'ether'));
-
             console.log("-------- claim rewards -------- ");
             return contracts.synthx.claimReward();
         })
         .then((receipt) => {
             console.log('synthx.claimReward receipt: ', receipt);
-
-            return contracts.synthxToken.balanceOf(accounts[0]);
         })
-        .then((balance) => {
-            console.log("synthx balance:", Web3Utils.fromWei(balance, 'ether'));
-
+        .then(() => {
             console.log("-------- trade -------- ");
             // dUSD => dTSLA
             return contracts.synthx.trade(Web3Utils.fromAscii('dUSD'), Web3Utils.toWei('1', 'ether'), Web3Utils.fromAscii('dTSLA'));
