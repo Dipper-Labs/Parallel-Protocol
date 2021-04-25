44a5275e5a6cac7b348a6a1bc7bdf718edb12324
cpucorecore <cpucorecore@gmail.com>
2021-04-25 17:55:04 +0800

    add BSC provider

--- truffle-config.js
+++ truffle-config.js
@@ -15,7 +15,13 @@ module.exports = {
     ropsten: {
       provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
       network_id: 3,
-      gas: 3000000,
+      gas: 9000000,
+      gasPrice: 10000000000
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://data-seed-prebsc-1-s1.binance.org:8545/"),
+      network_id: "*",
+      gas: 9000000,
       gasPrice: 10000000000
     },
   },
