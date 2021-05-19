35868c28e57280ef0643ad8d7d1576d69a0905f3
cpucorecore <cpucorecore@gmail.com>
2021-05-19 15:24:03 +0800

    update 104_setMaxDelayTime.js

--- migrations_manual/104_setMaxDelayTime.js
+++ migrations_manual/104_setMaxDelayTime.js
@@ -2,13 +2,16 @@ const contractAddrs = require('../finalContractAddrs.json')
 
 const AssetPrice = artifacts.require("AssetPrice");
 
-module.exports = async function(deployer, network, accounts) {
+// truffle migrate --network local --skip-dry-run -f 104 --maxDelayTime=360000
+const args = require('minimist')(process.argv.slice(2));
+
+module.exports = async function(deployer) {
 
     const assetPrice = await AssetPrice.at(contractAddrs.assetPrice);
 
     await deployer
         .then(() => {
-            return assetPrice.setMaxDelayTime(360000);
+            return assetPrice.setMaxDelayTime(args['maxDelayTime']);
         })
         .then((receipt) => {
             console.log('receipt: ', receipt);
